19a1e319c1c42515b9961995baec0ecbcf28c7ae
cawauchi6204 <cawauchi6204@gmail.com>
2020-07-04 00:32:45 +0900

    Sidebarコンポーネントの作成

---
+++ components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-4">
+    <figure class="image is-square">
+      <img class="is-rounded" src="https://identicons.github.com/pronama.png" />
+    </figure>
+    <h3>taro yamada</h3>
+    <p>職業:無職</p>
+    <p>email:hogehoge@example.com</p>
+    <p>hogehgoefugafuga</p>
+    <button class="button is-gray">Edit profile</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
